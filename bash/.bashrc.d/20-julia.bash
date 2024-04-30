b366568431f8d3641b867c1eaf50f3988f907174
Nicu Stiurca <nstiurca@pm.me>
2024-04-30 06:14:38 -0500

    Add Julia and ROS Noetic bashrc fragments

---
+++ bash/.bashrc.d/20-julia.bash
@@ -0,0 +1,17 @@
+if [ -d "$HOME/.juliaup/bin" ]; then
+# >>> juliaup initialize >>>
+
+# !! Contents within this block are managed by juliaup !!
+
+case ":$PATH:" in
+    *:$HOME/.juliaup/bin:*)
+        ;;
+
+    *)
+        export PATH=$HOME/.juliaup/bin${PATH:+:${PATH}}
+        ;;
+esac
+
+# <<< juliaup initialize <<<
+fi
+
---
+++ bash/.bashrc.d/81-ros-noetic.bash
@@ -0,0 +1,4 @@
+if [ -f /opt/ros/noetic/setup.bash ]; then
+    source /opt/ros/noetic/setup.bash
+fi
+
