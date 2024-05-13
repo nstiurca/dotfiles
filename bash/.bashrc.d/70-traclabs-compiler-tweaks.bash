27b077d5be5130b29deb2d706b9a81eea0465a27
Nicu Stiurca <nstiurca@pm.me>
2024-05-13 11:46:58 -0500

    Limit traclabs compiler tweaks to appropriate hosts

--- bash/.bashrc.d/70-traclabs-compiler-tweaks.bash
+++ bash/.bashrc.d/70-traclabs-compiler-tweaks.bash
@@ -1,18 +1,22 @@
-export CFLAGS="-fstack-protector-strong -Wformat -Werror=format-security"
-#export CFLAGS="$CFLAGS -Wall -Wextra"
-export CFLAGS="$CFLAGS -g -fno-limit-debug-info -fno-omit-frame-pointer"  # some nice debugging flags
-# export CFLAGS="$CFLAGS -march=native" # this seems to cause bad crashes with Eigen stuff :skull-bones:
+case $TOOLBX_NAME.$HOSTNAME in
+    .shadowfax)
+        export CFLAGS="-fstack-protector-strong -Wformat -Werror=format-security"
+        # export CFLAGS="$CFLAGS -Wall -Wextra"
+        export CFLAGS="$CFLAGS -g -fno-limit-debug-info -fno-omit-frame-pointer"  # some nice debugging flags
+        # export CFLAGS="$CFLAGS -march=native" # this seems to cause bad crashes with Eigen stuff :skull-bones:
 
-export CXXFLAGS="$CFLAGS -Wno-inconsistent-missing-override"
+        export CXXFLAGS="$CFLAGS -Wno-inconsistent-missing-override"
 
-export CMAKE_C_FLAGS_DEBUG="-Og $CFLAGS"
-export CMAKE_CXX_FLAGS_DEBUG="-Og $CXXFLAGS"
-export CMAKE_C_FLAGS_RELWITHDEBINFO="-O2 $CFLAGS"
-export CMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 $CXXFLAGS"
-export CMAKE_BUILD_TYPE="RelWithDebInfo"
-export CMAKE_CONFIGURATION_TYPES="RelWithDebInfo;Debug"
-export CMAKE_EXPORT_COMPILE_COMMANDS="TRUE"
+        export CMAKE_C_FLAGS_DEBUG="-Og $CFLAGS"
+        export CMAKE_CXX_FLAGS_DEBUG="-Og $CXXFLAGS"
+        export CMAKE_C_FLAGS_RELWITHDEBINFO="-O2 $CFLAGS"
+        export CMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 $CXXFLAGS"
+        export CMAKE_BUILD_TYPE="RelWithDebInfo"
+        export CMAKE_CONFIGURATION_TYPES="RelWithDebInfo;Debug"
+        export CMAKE_EXPORT_COMPILE_COMMANDS="TRUE"
+	use_clang
+        ;;
+esac
 
-use_clang
 use_ccache
 
