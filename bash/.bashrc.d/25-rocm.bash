850b9f2fa5ee1ae94f6a3c8d7cdbd7db3a8eeac6
Nicu Stiurca <nstiurca@pm.me>
2025-06-29 00:35:03 -0500

    ROCm env setup improvements; first pass at rocm7 support

--- bash/.bashrc.d/25-rocm.bash
+++ bash/.bashrc.d/25-rocm.bash
@@ -1,6 +1,10 @@
 if [ -f /opt/rocm/bin/rocminfo ]; then
-    export PATH="/opt/rocm/bin:$PATH"
+    export ROCM_PATH=/opt/rocm
+    export PATH="${ROCM_PATH}/bin:$PATH"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ROCM_PATH}/lib"
     # rocm has shoddy official support for gfx1103 (780m), and this helps, maybe
     export HSA_OVERRIDE_GFX_VERSION=11.0.0
+    export HSA_ENABLE_SDMA=0
 fi
+export THEROCK_AMDGPU_FAMILIES=gfx110X-igpu
 
